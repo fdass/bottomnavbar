43935819c3b03357708d0fef8423797ffb846f83
fdass <frederik.cs16@gmail.com>
2018-03-17 22:33:56 +0700

    first commit

---
+++ app/src/main/java/com/kompress/ayobeasiswa/tabmenufooter/BottomMenu.java
@@ -0,0 +1,47 @@
+package com.kompress.ayobeasiswa.tabmenufooter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class BottomMenu extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_beranda);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_cari);
+                    return true;
+                case R.id.navigation_histori:
+                    mTextMessage.setText(R.string.title_histori);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.title_akun);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_menu);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
