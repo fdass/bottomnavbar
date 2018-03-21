e6f69c27d0c0000274c0d1ae66827b3578333bf3
fdass <frederik.cs16@gmail.com>
2018-03-21 23:16:55 +0700

    SecondAttempt

---
+++ app/src/main/java/com/kompress/ayobeasiswa/tabmenufooter/ActivitySearch.java
@@ -0,0 +1,118 @@
+package com.kompress.ayobeasiswa.tabmenufooter;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class ActivitySearch extends Fragment implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+    private TextView mDisplayDate;
+    private TextView butto;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private Spinner spNamen2;
+    private String[] germanFeminine = {
+            "Negeri",
+            "Penelitian",
+            "Mahasiswa",
+            "Yayasan",
+            "Bantuan",
+            "Penghargaan",
+            "Luar Negeri",
+            "Olahraga",
+            "Swasta",
+            "Komputer"
+    };
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }*/
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_main,container, false);
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        View v = getView();
+        mDisplayDate = v.findViewById(R.id.textView2);
+        spNamen2 = v.findViewById(R.id.spinner);
+
+        // inisialiasi Array Adapter dengan memasukkan string array di atas
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_spinner_item, germanFeminine);
+
+        // mengeset Array Adapter tersebut ke Spinner
+        spNamen2.setAdapter(adapter);
+
+        // mengeset listener untuk mengetahui saat item dipilih
+        spNamen2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                // memunculkan toast + value Spinner yang dipilih (diambil dari adapter)
+                Toast.makeText(getActivity(), "Selected "+ adapter.getItem(i), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //tanggalannya
+        mDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        getActivity(),
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year, month, day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG, "onDateSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate.setText(date);
+            }
+        };
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/kompress/ayobeasiswa/tabmenufooter/BottomMenu.java
+++ app/src/main/java/com/kompress/ayobeasiswa/tabmenufooter/BottomMenu.java
@@ -1,35 +1,64 @@
 package com.kompress.ayobeasiswa.tabmenufooter;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class BottomMenu extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private static final String TAG = "SearchActivity";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            /*Fragment fragment = null;
+            Class fragmentClass = null;
+            */
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_beranda);
+                    //mTextMessage.setText(R.string.title_beranda);
                     return true;
                 case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_cari);
+                    //mTextMessage.setText(R.string.title_cari);
+                    Log.v(TAG,"Changing class...");
+
+                    //fragmentClass = ActivitySearch.class;
+
+                    ActivitySearch details = new ActivitySearch();
+                    FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    ft.replace(R.id.fragContainer,details);
+                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                    ft.commit();
+
                     return true;
                 case R.id.navigation_histori:
-                    mTextMessage.setText(R.string.title_histori);
+                    //mTextMessage.setText(R.string.title_histori);
                     return true;
                 case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_akun);
+                    //mTextMessage.setText(R.string.title_akun);
                     return true;
             }
+
+            /*try {
+                fragment = (Fragment) fragmentClass.newInstance();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+            FragmentManager fm = getFragmentManager();
+            fm.beginTransaction().replace(R.id.fragContainer,fragment).commit();*/
+
             return false;
         }
     };
